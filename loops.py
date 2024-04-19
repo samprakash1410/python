ef2474c14e9b19ac2b9afd38cb7967d97800a3a6
samprakash.R <samprakash1410@gmail.com>
2024-04-19 17:44:21 +0530

    adde loops

---
+++ loops.py
@@ -0,0 +1,7 @@
+#day of the first week starts
+weekdays = ["Sunday", "Monday", "Tuesday",
+            "Wednesday", "Thursday", "Friday", "Saturday"]
+print("Seven Weekdays are:\n")
+# Iterate the list using for loop
+for day in range(len(weekdays)):
+    print(weekdays[day])
