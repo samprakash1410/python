c31c97b925a9e1867853e279a054673cdb2b7bca
samprakash.R <samprakash1410@gmail.com>
2024-04-20 15:18:23 +0530

    added changes

--- function.py
+++
@@ -1,6 +0,0 @@
-def max_of_two(x, y):
-    
-    if x > y:
-        return x
-    else:
-        return y
--- sample.py
+++
@@ -1,6 +0,0 @@
-# Importing required module
-import subprocess
-
-# Using system() method to
-# execute shell commands
-subprocess.Popen('echo "Nicky 4 sam"', shell=True)
