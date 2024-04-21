12daf3cfb63c7eba0abe4da306488dba4a83a3bb
samprakash.R <samprakash1410@gmail.com>
2024-04-21 11:53:37 +0530

    added changes

---
+++ if elif else.py
@@ -0,0 +1,13 @@
+letter = "D"
+
+if letter == "B":
+    print("letter is B")
+    
+elif letter =="C":
+    print("letter is D")
+    
+elif letter =="A":
+    print("letter is A")
+    
+else:
+    print("letter is not correct A, B & D")
